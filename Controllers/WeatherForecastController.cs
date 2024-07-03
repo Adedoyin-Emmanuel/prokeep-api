e2d86b56122cde76ac9e7167c43798c1dab30184
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-07-03 14:54:15 +0100

    feat: added product model and dbcontext

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace prokeep.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
