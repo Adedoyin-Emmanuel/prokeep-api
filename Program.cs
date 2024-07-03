5b163abd4e15a23edaa0933d5ab13d0524bb948d
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-07-03 15:54:09 +0100

    fix: changed env DB_CONNENCTION_STRING to fit Npgsql connection string format

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING"
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddDbContextPool<AppContext>(options => options.UseNpgsql(connectionString));
+builder.EnrichNpgsqlDbContext<AppContext>();
 
 var app = builder.Build();
 
