0a3df3da2b8f64c8706859f1bed70ecb4564d00c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-07-03 15:06:36 +0100

    feat: finished product model

---
+++ Models/ProductModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace prokeep.Models;
+
+
+public class Product
+{
+
+    public Guid Id { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public int Price { get; set; }
+    public required string Manufacturer { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime CreatedAt { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTime UpdatedAt { get; set; }
+}
